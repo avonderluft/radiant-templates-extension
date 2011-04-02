218f724baa85e1d52792016307bf8c8ae503c3d1
Andrew vonderLuft <avonderluft@avlux.net>
2011-04-01 21:17:40 -0700

    move template selector out of drawer onto main page-edit

--- templates_extension.rb
+++ templates_extension.rb
@@ -27,7 +27,7 @@ class TemplatesExtension < Radiant::Extension
     # Admin UI customization
     admin.page.index.add :node, 'template_column', :after => 'title_column'
     admin.page.index.add :sitemap_head, 'template_column_header', :after => 'title_column_header'
-    admin.page.edit.add :extended_metadata, 'switch_templates', :after => 'breadcrumb'
+    admin.page.edit.add :main, "template_chooser", :before => "edit_header"
     admin.page.edit.add :form, 'edit_template', :before => 'edit_page_parts'
     admin.page.edit.form.delete 'edit_page_parts'
     admin.page.edit.parts_bottom.delete 'edit_layout_and_type'
